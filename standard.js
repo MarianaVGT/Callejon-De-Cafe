f03ab40389b51dbb33b1de2a3cba61b5f6b2e9b9
MarianaVGT <116393953+MarianaVGT@users.noreply.github.com>
2022-10-21 18:19:01 -0500

    Add files via upload

---
+++ standard.js
@@ -0,0 +1,8 @@
+function clicked() {
+    var removeItem = document.getElementById("button");
+    var value = document.createElementById("add").value;
+    li.appendChild(textNode);
+    list.appendChild(li)
+    button.onclick = removeItem;
+    v
+}
